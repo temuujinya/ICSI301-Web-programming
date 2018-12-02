40185d58c7f4e1d6883e968f09aad2e39bb1520f
USOTT <y.usott@gmail.com>
2018-12-02 20:57:51 +0800

    finished lab5 drawing game

--- lab5/assets/js/draw.js
+++ lab5/assets/js/draw.js
@@ -1,18 +1,8 @@
-
+// if(document.readyState =='interactive'){
 colorClasses = ["bg-yellow", "bg-red", "bg-black", 
 "bg-lightgreen", "bg-blue"];
-
-
-let smile=[
-    ["","0","0","0","0","0",""],
-    ["0","","","","","","0"],
-    ["0","","2","","2","","0"],
-    ["0","","","","","","0"],
-    ["0","","","","","","0"],
-    ["0","","1","1","1","","0"],
-    ["0","","","","","","0"],
-    ["","0","0","0","0","0",""],
-];
+let inCorrect=[[]];
+// error.push([1])
 // let smile=[
 //     ["","bg-yellow","bg-yellow","bg-yellow","bg-yellow","bg-yellow",""],
 //     ["bg-yellow","","","","","","bg-yellow"],
@@ -23,6 +13,52 @@ let smile=[
 //     ["bg-yellow","","","","","","bg-yellow"],
 //     ["","bg-yellow","bg-yellow","bg-yellow","bg-yellow","bg-yellow",""],
 // ];
+
+let smile=[
+    [-1,0,0,0,0,0,-1],
+    [0,-1,-1,-1,-1,-1,0],
+    [0,-1,2,-1,2,-1,0],
+    [0,-1,-1,-1,-1,-1,0],
+    [0,-1,-1,-1,-1,-1,0],
+    [0,-1,1,1,1,-1,0],
+    [0,-1,-1,-1,-1,-1,0],
+    [-1,0,0,0,0,0,-1,]
+];
+
+let userinput=[
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+    [-1,-1,-1,-1,-1,-1,-1],
+];
+
+function drawMission(drawField,missionArr){
+    draw = document.querySelector(drawField)
+
+    for(let i=0; i<smile.length;i++){
+        s = document.createElement("tr");
+        for(let j=0; j<smile[i].length;j++){
+            tdata = document.createElement("td");
+            tdataAttr = document.createAttribute("number");
+            tdataAttr.value= i*10+j;
+            tdata.setAttributeNode(tdataAttr);
+            if(missionArr != undefined)
+            tdata.className = findColorClass(smile[i][j]);
+            
+            s.appendChild(tdata);
+        }
+        draw.appendChild(s);
+    }
+}
+
+drawMission("#challenge #mission tbody",smile);
+drawMission("#challenge #userBoard tbody");
+
+
 
 
 // bg-yellow
@@ -47,5 +83,83 @@ function findColorClass(colorNumber){
         case 4: 
             return colorClasses[colorNumber];
             break;
+        default: return " ";break;
     }
 }
+
+
+function checkCorrectColor(missionArr){
+    if(JSON.stringify(missionArr)===JSON.stringify(userinput)){
+        // if(inCorrect!==false)
+        // inCorrect=true;
+        alert("hojloo");
+        document.location.reload();
+    }else{
+        console.log("loose");
+        alert("looser");
+        document.location.reload();
+    }
+    console.log(inCorrect);
+}
+
+let currentColor = null;
+
+let userBoard = document.querySelectorAll("#challenge #userBoard tbody tr td");
+
+userBoard.forEach((onePixel)=>{
+    onePixel.addEventListener("click",(e)=>{
+        colorNumber=parseInt(currentColor);
+        pixelNumber = parseInt(onePixel.getAttribute("number"));
+        if(currentColor!=null){
+            onePixel.classList=findColorClass(colorNumber);
+            userinput[parseInt(pixelNumber/10)][parseInt(pixelNumber%10)]=colorNumber;
+            // checkCorrectColor(smile);            
+        }else{
+            alert("Өнгөө сонго чөөгий");
+        }
+        
+    },false);
+    onePixel.addEventListener("dblclick", ()=>{
+        colorNumber=parseInt(currentColor);
+        pixelNumber = parseInt(onePixel.getAttribute("number"));
+        if(onePixel.classList!=null){
+            onePixel.classList="";
+            userinput[parseInt(pixelNumber/10)][parseInt(pixelNumber%10)]=-1;
+        }
+    },false);
+});
+
+
+let colorBtns = document.querySelectorAll(".color-btn");
+
+colorBtns.forEach((colorBtn)=>{
+    colorBtn.addEventListener("click",()=>{
+        currentColor = colorBtn.getAttribute("colorCode");
+    },false)
+})
+
+gametime=3;
+seconds =gametime;
+startbtn = document.getElementById("startBTN");
+
+var myVar = null;
+function myTimer() {
+    if(seconds===0 || seconds<=0){
+        checkCorrectColor(smile);
+        clearInterval(myVar);
+    }
+    document.getElementById("timer").innerHTML = seconds;
+    --seconds;
+}
+
+startbtn.addEventListener("click",()=>{
+    if(myVar!==null){
+        clearInterval(myVar);
+    }
+    seconds=gametime;
+    myVar = setInterval(myTimer, 1000);
+},false);
+
+
+
+// }
