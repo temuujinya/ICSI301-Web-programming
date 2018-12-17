7676feb8ba7cae5bd9f32d4d3293f56b26febd3b
Temka <y.usott@gmail.com>
2018-12-17 10:38:08 -0500

    added checkusername.php

---
+++ lab7/include/checkUsername.php
@@ -0,0 +1,17 @@
+<?php 
+
+require "./init.php";
+if(isset($_GET["username"])){
+    $username = $_GET["username"];
+
+    header("Content-Type: text/xml");
+    echo "<?xml version=\"1.0\"?>\n";
+    echo "<found>";
+    if(loginCheckUsername($username)){
+        echo "true";
+    }else{
+        echo "false";
+    }
+    echo "</found>";
+
+}
